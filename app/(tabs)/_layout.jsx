0ba0ffd6a6d6d6f0cb0fbf684599dddeffe0d7ea
devavinothm <learnerdev101@gmail.com>
2024-09-16 23:15:12 +0530

    Converting all .tsx files into .jsx files

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,17 +1,13 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import TabBarIcon from '../../components/navigation/TabBarIcon';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
       }}>
       <Tabs.Screen
